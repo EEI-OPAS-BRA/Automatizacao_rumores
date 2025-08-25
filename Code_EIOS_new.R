dc3b6b748d01cde1b0a845ad0538d4994866517a
cleoah <90575087+cleoah@users.noreply.github.com>
2025-08-25 14:43:38 -0300

    update

--- Code_EIOS_new.R
+++ Code_EIOS_new.R
@@ -1,14 +1,12 @@
 library(pacman)
 pacman::p_load(tidyverse,
-               stopwords,
-               xml2,
+               stopwords,#limpeza de texto, deixar em pt
+               xml2, #trabalhar com HTML e XML
                httr,
-               rvest,
-               tidytext,
-               wordcloud2,
-               officer,
+               tidytext,#mineração de texto
                lubridate,
                stringi,
+               stringr,
                writexl,
                officer,
                zip)
@@ -32,14 +30,13 @@ get_rss_data <- function(url) {
 urls <- c(
   "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=12760",
   "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=13428",
-  "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=12847",
+  "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=17593",
   "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=13620",
   "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=11151",
-  "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=13462",
-  "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=2478",
-  "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=14901",
-  "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=13891"
+  "https://portal.who.int/eios/API/News/Monitoring/getBoardRssFeed?queryId=17828"
 )
+
+
 
 # Nomes dos termos associados a cada URL
 termos <- c(
@@ -48,11 +45,11 @@ termos <- c(
   'Vírus Respiratorios',
   'Ameaças Naturais',
   'Arboviroses',
-  'Refugiados Israel',
-  'Outros',
-  'Doenças Gerais',
-  'Sintomas'
+  "Vacinação"
 )
+
+
+
 
 # Baixar e combinar os dados dos feeds RSS
 df_list <- map(urls, get_rss_data)
@@ -74,9 +71,16 @@ df_final <- df_final %>%
 
 # Lista de palavras-chave
 palavras_chave <- c("casos", "contra", "alerta", "doenca", "aumento", "grande", "emergencia", "numero", "situacao",
-                    "mortes", "caso", "vacina", "registra", "maior", "chuvas", "dados", "surto", "confirmados",
-                    "confirma", "morte","obitos","obito", "virus", "alta", "risco", "vigilancia", "seca", "leitos", "registrou",
-                    "chuva", "causa","queimada","queimadas","estiagem","quente","hospitalizacao","internacao","SRAG")
+                    "mortes", "caso", "vacina","vacinacao","campanha vacinacao","doses", "registra", "maior", "chuvas", "dados", "surto", "confirmados","gripe","virus respiratorios",
+                    "confirma", "morte", "obitos", "obito", "virus", "alta", "risco", "vigilancia", "seca", "leitos", 
+                    "registrou", "chuva", "causa", "queimada", "queimadas", "estiagem", "quente", "hospitalizacao", 
+                    "internacao", "SRAG","VSR", "influenza aviaria","aviaria","H5N1","rinovirus","covid","infogripe","gripe aviária","EUA","Síndrome Respiratória Aguda Grave","sobrecarregam","sobrecarregar")
+
+
+# Filter news based on keywords in the title only
+
+# df_final <- df_final[grepl(paste(palavras_chave, collapse = "|"), df_final$Title, ignore.case = TRUE), ]
+
 
 # Função para calcular a pontuação com base nas palavras-chave
 calcular_pontuacao <- function(texto) {
@@ -86,221 +90,124 @@ calcular_pontuacao <- function(texto) {
   return(pontuacao)
 }
 
-# Calcular pontuação e filtrar resultados
+# Calcular pontuação, filtrar resultados e remover duplicatas
 df_final <- df_final %>%
   mutate(Pontuacao = map_int(Title_limpo, calcular_pontuacao)) %>%
   arrange(desc(Pontuacao))
 
-df_final_filtrado <- df_final %>% filter(Pontuacao >= 3)
+
+df_final_filtrado <- df_final %>%
+  filter(Pontuacao >= 2) %>%
+  distinct(Title, .keep_all = TRUE)
 
 
-# Lista de estados, capitais, siglas e outros termos em uma linha
-estados_capitais_siglas <- c(
-  'Acre', 'Rio Branco', 'AC', 'Alagoas', 'Maceio', 'AL', 'Amapa', 'Macapa', 'AP', 'Amazonas', 'Manaus', 'AM',
-  'Bahia', 'Salvador', 'BA', 'Ceara', 'Fortaleza', 'CE', 'Distrito Federal', 'Brasilia', 'DF', 'Espirito Santo', 'Vitoria', 'ES',
-  'Goias', 'Goiania', 'GO', 'Maranhao', 'Sao Luis', 'MA', 'Mato Grosso', 'Cuiaba', 'MT', 'Mato Grosso do Sul', 'Campo Grande', 'MS',
-  'Minas Gerais', 'Belo Horizonte', 'MG', 'Para', 'Belem', 'PA', 'Paraiba', 'Joao Pessoa', 'PB', 'Parana', 'Curitiba', 'PR',
-  'Pernambuco', 'Recife', 'PE', 'Piaui', 'Teresina', 'PI', 'Rio de Janeiro', 'Rio de Janeiro', 'RJ', 'Rio Grande do Norte', 'Natal', 'RN',
-  'Rio Grande do Sul', 'Porto Alegre', 'RS', 'Rondonia', 'Porto Velho', 'RO', 'Roraima', 'Boa Vista', 'RR', 'Santa Catarina', 'Florianopolis', 'SC',
-  'Sao Paulo', 'Sao Paulo', 'SP', 'Sergipe', 'Aracaju', 'SE', 'Tocantins', 'Palmas', 'TO', 'Brasil', 'Brasilia', 'BR', 'Ministerio da Saude','Governo'
-)
 
-# Função para verificar se o título contém algum estado, capital ou sigla
-contém_termos <- function(titulo, termos) {
-  any(str_detect(titulo, termos))
-}
-
-# Atualizar o DataFrame com a coluna booleana
+# Atualizar nomes das colunas e remover a coluna 'Title_limpo'
 df_final_filtrado <- df_final_filtrado %>%
-  mutate(
-    Contém_Termos = map_lgl(Title_limpo, ~ contém_termos(.x, estados_capitais_siglas))
-  ) %>%
-  filter(Contém_Termos) %>%
-  select(-Contém_Termos) %>%  # Remover a coluna booleana se não for mais necessária
-  distinct(Title_limpo, .keep_all = TRUE)  # Remover duplicatas com base na coluna Title_limpo
+  select(
+    Título = Title,
+    Link,
+    `Data da Publicação` = Published,
+    Descrição = Description,
+    Categoria = Termo)
 
 
 
-library(officer)
-library(dplyr)
-library(stringr)
 
-library(officer)
-library(dplyr)
-library(stringr)
+# # Assuming df_final_filtrado is your existing dataframe
+# titles <- df_final_filtrado$Título
+# 
+# # Preprocess the text
+# preprocess_text <- function(text) {
+#   text <- tolower(text)
+#   text <- removePunctuation(text)
+#   text <- removeWords(text, stopwords("portuguese"))
+#   return(text)
+# }
+# 
+# titles_clean <- sapply(titles, preprocess_text)
+# 
+# # Calculate Jaccard similarity
+# similarity_matrix <- stringdistmatrix(titles_clean, titles_clean, method = "jaccard")
+# 
+# # Set a threshold for similarity (e.g., 0.2)
+# threshold <- 0.2
+# 
+# # Find duplicates
+# duplicates <- which(similarity_matrix < threshold, arr.ind = TRUE)
+# duplicates <- duplicates[duplicates[,1] != duplicates[,2],]
+# 
+# # Include length of the titles in the comparison
+# title_lengths <- nchar(titles)
+# 
+# # Function to keep the longer title
+# keep_longer_title <- function(dup_indices, lengths) {
+#   to_remove <- c()
+#   for (i in 1:nrow(dup_indices)) {
+#     if (lengths[dup_indices[i, 1]] >= lengths[dup_indices[i, 2]]) {
+#       to_remove <- c(to_remove, dup_indices[i, 2])
+#     } else {
+#       to_remove <- c(to_remove, dup_indices[i, 1])
+#     }
+#   }
+#   return(unique(to_remove))
+# }
+# 
+# # Get indices of titles to remove
+# to_remove <- keep_longer_title(duplicates, title_lengths)
+# 
+# # Remove duplicates
+# unique_titles <- titles[-to_remove]
 
-# Carregar o modelo existente
-doc <- read_docx("Modelo_word.docx")
+# Create a new dataframe with unique titles
+# df_unique <- df_final_filtrado %>% filter(Título %in% unique_titles)
 
-# Configurar o estilo para a data com Franklin Gothic e negrito
-data_estilo <- fp_text(font.size = 12, font.family = "Franklin Gothic", bold = TRUE)
 
-# Obter a data atual e formatá-la com o mês em maiúsculas
-data_atual <- format(Sys.Date(), "%d de %B")
-data_atual <- str_to_upper(data_atual)  # Colocar o mês em maiúsculas
+df_final_filtrado <- df_final_filtrado[, c("Categoria", "Título", "Link", "Data da Publicação", "Descrição")]
 
-# Adicionar a data antes das notícias, alinhada à direita, com o estilo configurado
-doc <- doc %>%
-  body_add_fpar(
-    fpar(
-      ftext(data_atual, prop = data_estilo),
-      fp_p = fp_par(text.align = "right")
-    )
-  ) %>%
-  body_add_par("", style = "Normal")  # Espaço após a data
 
-# Imagens específicas para cada categoria
-imagens_categorias <- list(
-  'Surtos e Emergências' = "logo_surtos.png",
-  'Secas e Queimadas' = "logo_secas_queimadas.png",
-  'Vírus Respiratorios' = "logo_virus_respiratorios.png",
-  'Ameaças Naturais' = "logo_ameacas_naturais.png",
-  'Arboviroses' = "logo_arboviroses.png",
-  'Refugiados Israel' = "logo_refugiados_israel.png",
-  'Outros' = "logo_outros.png",
-  'Doenças Gerais' = "logo_doencas_gerais.png",
-  'Sintomas' = "logo_sintomas.png"
-)
 
-# Largura e altura desejada para as imagens
-largura_imagem_cm <- 15.0  # Ajuste a largura conforme necessário (em cm)
-altura_imagem_cm <- 1.75   # Altura desejada em cm
 
-# Função para adicionar imagens com tamanho fixo
-adicionar_imagem <- function(doc, imagem_path, largura_cm, altura_cm) {
-  largura_inch <- largura_cm / 2.54
-  altura_inch <- altura_cm / 2.54
-  doc <- body_add_img(doc, src = imagem_path, width = largura_inch * 72, height = altura_inch * 72)
-  return(doc)
+df_final_filtrado$Descrição <- str_wrap(df_final_filtrado$Descrição, width = 60)
+
+wb <- createWorkbook()
+addWorksheet(wb, "Sheet1")
+
+writeData(wb, "Sheet1", df_final_filtrado, startCol = 1, startRow = 1)
+
+
+for (i in seq_along(df_final_filtrado$Link)) {
+  writeFormula(
+    wb, "Sheet1",
+    startRow = i + 1, startCol = 3,
+    x = sprintf('=HYPERLINK("%s", "%s")',  df_final_filtrado$Link[i], df_final_filtrado$Link[i])
+  )
 }
 
-# Adicionar as notícias divididas por categorias
-for (termo in termos) {
-  # Filtrar as notícias dessa categoria
-  noticias_categoria <- df_final_filtrado %>% filter(Termo == termo)
-  
-  # Se houver notícias na categoria, adiciona a imagem da categoria e as notícias
-  if (nrow(noticias_categoria) > 0) {
-    # Adicionar a imagem correspondente à categoria
-    imagem_categoria <- imagens_categorias[[termo]]
-    if (!is.null(imagem_categoria)) {
-      doc <- adicionar_imagem(doc, imagem_categoria, largura_imagem_cm, altura_imagem_cm)
-    }
-    
-    for (i in seq_len(nrow(noticias_categoria))) {
-      noticia <- noticias_categoria[i, ]
-      
-      # Adicionar título, descrição e link com estilos personalizados
-      doc <- doc %>%
-        body_add_fpar(fpar(ftext(noticia$Title, prop = titulo_estilo))) %>%
-        body_add_fpar(fpar(ftext(noticia$Description, prop = descricao_estilo))) %>%
-        body_add_fpar(fpar(ftext("Link", prop = link_estilo), hyperlink = noticia$Link)) %>%
-        body_add_par("", style = "Normal")  # Adiciona uma linha em branco para separação
-    }
-  }
-}
 
-# Salvar o documento modificado
-print(doc, target = "noticias_filtradas_modelo.docx")
 
-# Carregar o modelo existente
-doc <- read_docx("Modelo_word.docx")
+data_atual <- format(Sys.Date(), "%d-%m-%Y")
 
-# Configurar o estilo para a data com Franklin Gothic e negrito
-data_estilo <- fp_text(font.size = 12, font.family = "Franklin Gothic", bold = TRUE)
+# Definir o caminho completo do diretório e nome do arquivo
+caminho <- "C:/Users/andradecle/OneDrive - Pan American Health Organization/General - PHE BRA/Rumores/Automatização/"
 
-# Configurar o estilo para o título
-titulo_estilo <- fp_text(font.size = 15, font.family = "Franklin Gothic", bold = TRUE)
+nome_arquivo <- paste0(caminho, "Rumores_", data_atual, ".xlsx")
 
-# Configurar o estilo para a descrição
-descricao_estilo <- fp_text(font.size = 11, font.family = "Franklin Gothic")
+# Exportar o data frame para o arquivo CSV no caminho especificado
 
-# Configurar o estilo para o link
-link_estilo <- fp_text(font.size = 10, font.family = "Franklin Gothic", color = "blue", underline = TRUE)
-
-# Obter a data atual e formatá-la com o mês em maiúsculas
-data_atual <- format(Sys.Date(), "%d de %B")
-data_atual <- str_to_upper(data_atual)  # Colocar o mês em maiúsculas
-
-# Adicionar a data antes das notícias, alinhada à direita, com o estilo configurado
-doc <- doc %>%
-  body_add_fpar(
-    fpar(
-      ftext(data_atual, prop = data_estilo),
-      fp_p = fp_par(text.align = "right")
-    )
-  ) %>%
-  body_add_par("", style = "Normal")  # Espaço após a data
-
-# Mapear categorias para imagens
-imagens <- c(
-  'Surtos e Emergências' = "logo_surtos.png",
-  'Secas e Queimadas' = "logo_secas_queimadas.png",
-  'Vírus Respiratorios' = "logo_virus_respiratorios.png",
-  'Ameaças Naturais' = "logo_ameacas_naturais.png",
-  'Arboviroses' = "logo_arboviroses.png",
-  'Refugiados Israel' = "logo_refugiados_israel.png",
-  'Outros' = "logo_outros.png",
-  'Doenças Gerais' = "logo_doencas_gerais.png",
-  'Sintomas' = "logo_sintomas.png"
-)
-
-# Adicionar as notícias divididas por categorias
-for (termo in termos) {
-  # Filtrar as notícias dessa categoria
-  noticias_categoria <- df_final_filtrado %>% filter(Termo == termo)
-  
-  # Se houver notícias na categoria, adiciona a imagem ou o título e as notícias
-  if (nrow(noticias_categoria) > 0) {
-    if (termo %in% names(imagens)) {
-      # Adicionar a imagem correspondente à categoria
-      imagem_path <- imagens[[termo]]
-      doc <- doc %>%
-        body_add_fpar(
-          fpar(
-            external_img(src = imagem_path, width = 6.5, height = 0.75)  # Ajuste a altura conforme necessário
-          )
-        )
-    } else {
-      # Adicionar o título para categorias não listadas nas imagens
-      doc <- doc %>%
-        body_add_par(termo, style = "heading 1")
-    }
-    
-    doc <- doc %>%
-      body_add_par("", style = "Normal") # Espaço após o título ou imagem
-    
-    for (i in seq_len(nrow(noticias_categoria))) {
-      noticia <- noticias_categoria[i, ]
-      
-      # Adicionar título, descrição e link com estilos personalizados
-      doc <- doc %>%
-        body_add_fpar(fpar(ftext(noticia$Title, prop = titulo_estilo))) %>%
-        body_add_fpar(fpar(ftext(noticia$Description, prop = descricao_estilo))) %>%
-        body_add_fpar(
-          fpar(
-            ftext("Link", prop = link_estilo),
-            hyperlink = noticia$Link  # Adiciona o hyperlink diretamente ao URL
-          )
-        ) %>%
-        body_add_par("", style = "Normal")  # Adiciona uma linha em branco para separação
-    }
-  }
-}
-
-# Salvar o documento modificado
-print(doc, target = "noticias_filtradas_modelo.docx")
+saveWorkbook(wb, nome_arquivo, overwrite = TRUE)
+# export(df_unique, nome_arquivo)
 
 
 
 
 
-
-
-# Salvar
-excel_file <- "feed_data_filtrado.xlsx"
-write_xlsx(df_final_filtrado, excel_file)
-
-
-
-
+# # Defina o arquivo original
+# excel_file <- "feed_data__filtrado.xlsx"
+# 
+# # Defina o caminho de destino, incluindo a data do dia no nome do arquivo
+# today_date <- Sys.Date()
+# destination <- paste0("C:/Users/andradecle/OneDrive - Pan American Health Organization/General - PHE BRA/Rumores/Automatização/feed_data_filtrado_", today_date, ".xlsx")
+# write_xlsx(df_unique, excel_file)
+# # Copie o arquivo para o destino
+# file.copy(excel_file, destination)
